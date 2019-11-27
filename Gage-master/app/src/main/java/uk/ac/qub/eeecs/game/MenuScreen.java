c1cfaa27f2ad88c418b819f91e8581f88f1dde27
pauric <40227531>
2019-11-27 12:59:55 +0000

    User Story O1(Creating a derived options screen class [D2]) User Story O2 (Adding an options screen button to the menu Screen[D1])

--- Gage-master/app/src/main/java/uk/ac/qub/eeecs/game/MenuScreen.java
+++ Gage-master/app/src/main/java/uk/ac/qub/eeecs/game/MenuScreen.java
@@ -12,7 +12,7 @@ import uk.ac.qub.eeecs.gage.engine.input.Input;
 import uk.ac.qub.eeecs.gage.engine.input.TouchEvent;
 import uk.ac.qub.eeecs.gage.ui.PushButton;
 import uk.ac.qub.eeecs.gage.world.GameScreen;
-import uk.ac.qub.eeecs.game.MatchAttax.CardDemoScreen;
+import uk.ac.qub.eeecs.game.MatchAttax.*;
 import uk.ac.qub.eeecs.game.miscDemos.DemoMenuScreen;
 import uk.ac.qub.eeecs.game.platformDemo.PlatformDemoScreen;
 import uk.ac.qub.eeecs.game.spaceDemo.SpaceshipSelectScreen;
@@ -113,8 +113,6 @@ public class MenuScreen extends GameScreen {
 
             if (mSpaceshipDemoButton.isPushTriggered())
                 mGame.getScreenManager().addScreen(new SpaceshipSelectScreen(mGame));
-            else if (mCardDemoButton.isPushTriggered())
-                mGame.getScreenManager().addScreen(new CardDemoScreen(mGame));
             else if (mPlatformDemoButton.isPushTriggered())
                 mGame.getScreenManager().addScreen(new PlatformDemoScreen(mGame));
             else if (mDemosButton.isPushTriggered())

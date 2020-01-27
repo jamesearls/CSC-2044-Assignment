0debcf88d5564ee8d1f4b9fe1cf706a2a3c3fa66
40227531 <pdonnelly549@qub.ac.uk>
2020-01-27 14:51:00 +0000

    Start of the card class

--- Gage-master/app/src/main/java/uk/ac/qub/eeecs/game/matchAttax/cards/Cards.java
+++ Gage-master/app/src/main/java/uk/ac/qub/eeecs/game/matchAttax/cards/Cards.java
@@ -1,4 +1,29 @@
 package uk.ac.qub.eeecs.game.matchAttax.cards;
-public class Cards {
+
+import android.graphics.Bitmap;
+
+import uk.ac.qub.eeecs.gage.engine.AssetManager;
+import uk.ac.qub.eeecs.gage.engine.ElapsedTime;
+import uk.ac.qub.eeecs.gage.engine.graphics.IGraphics2D;
+import uk.ac.qub.eeecs.gage.util.BoundingBox;
+import uk.ac.qub.eeecs.gage.util.GraphicsHelper;
+import uk.ac.qub.eeecs.gage.util.Vector2;
+import uk.ac.qub.eeecs.gage.world.GameScreen;
+import uk.ac.qub.eeecs.gage.world.LayerViewport;
+import uk.ac.qub.eeecs.gage.world.ScreenViewport;
+import uk.ac.qub.eeecs.gage.world.Sprite;
+
+
+public class Cards{
+    private static final int DEFAULT_CARD_HEIGHT = 100;
+    private static final int DEFAULT_CARD_WIDTH = 50;
+
+
+    private Bitmap cardPortrait;
+
+
+
     
 }
+
+

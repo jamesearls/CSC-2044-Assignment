682f1d771838c422adc51a9757430c9510413cd5
40227531 <pdonnelly549@qub.ac.uk>
2020-01-29 13:31:56 +0000

    Working on the card class - trying to load assets from JSON file

--- Gage-master/app/src/main/java/uk/ac/qub/eeecs/game/matchAttax/cards/Cards.java
+++ Gage-master/app/src/main/java/uk/ac/qub/eeecs/game/matchAttax/cards/Cards.java
@@ -14,16 +14,34 @@ import uk.ac.qub.eeecs.gage.world.ScreenViewport;
 import uk.ac.qub.eeecs.gage.world.Sprite;
 
 
-public class Cards{
+
+public class Cards extends Sprite{
     private static final int DEFAULT_CARD_HEIGHT = 100;
     private static final int DEFAULT_CARD_WIDTH = 50;
 
+    private final int amountOfCardsInDatabase = 40;
+
 
     private Bitmap cardPortrait;
+    private int overallValue;
+    private String league;
+    private String club;
+    private String firstName;
+    private String surname;
+    private Cards[] cardsInStore;
 
 
 
-    
+    public Cards(float x, float y, GameScreen gameScreen, int overallValue, String league, String club, String firstName, String surname) {
+        super(x, y, DEFAULT_CARD_WIDTH, DEFAULT_CARD_HEIGHT, null, gameScreen);
+
+        AssetManager assetManager = gameScreen.getGame().getAssetManager();
+        assetManager.loadAssets("txt/assets/Players.JSON");
+
+
+
+
+    }
 }
 
 

50520c83cc5e915eb76ed34096c8e2d7177aff08
40227531 <pdonnelly549@qub.ac.uk>
2020-01-30 17:11:57 +0000

    Working on the card class - trying to load assets from JSON file

--- Gage-master/app/src/main/java/uk/ac/qub/eeecs/game/matchAttax/cards/Cards.java
+++ Gage-master/app/src/main/java/uk/ac/qub/eeecs/game/matchAttax/cards/Cards.java
@@ -15,6 +15,7 @@ import uk.ac.qub.eeecs.gage.world.Sprite;
 
 
 
+
 public class Cards extends Sprite{
     private static final int DEFAULT_CARD_HEIGHT = 100;
     private static final int DEFAULT_CARD_WIDTH = 50;
@@ -28,15 +29,29 @@ public class Cards extends Sprite{
     private String club;
     private String firstName;
     private String surname;
-    private Cards[] cardsInStore;
+    private Cards[] cardsInStore = new Cards[amountOfCardsInDatabase];
 
 
 
-    public Cards(float x, float y, GameScreen gameScreen, int overallValue, String league, String club, String firstName, String surname) {
+    public Cards(float x, float y, GameScreen gameScreen, int overallValue, String league, String club, String firstName, String surname, Bitmap cardPortrait){
         super(x, y, DEFAULT_CARD_WIDTH, DEFAULT_CARD_HEIGHT, null, gameScreen);
 
-        AssetManager assetManager = gameScreen.getGame().getAssetManager();
-        assetManager.loadAssets("txt/assets/Players.JSON");
+
+            AssetManager assetManager = gameScreen.getGame().getAssetManager();
+            assetManager.loadAssets("txt/assets/Players.JSON");
+
+            for (int i = 0; i < amountOfCardsInDatabase; i++) {
+                //Below is comments of the code im trying to achieve
+                /*overallValue = assetManager.load___("overall");
+                league = assetManager.load___("league");
+                club = assetManager.load___("team")
+                firstName = assetManager.load____("fname");
+                surname = assetManager.load_____("sname");
+                cardPortrait = assetManager.loadBitmap("portrait");
+                cardsInStore[i] = {overallValue, league, club, firstName, surname, cardPortrait};
+
+                 */
+            }
 
 
 

6f4ac9939c9b1634a1910009661ad450fc671a3b
davidm9528 <davidm9528@gmail.com>
2020-02-20 14:43:05 +0000

    Small start on the Manager class (Try Catches)

--- Gage-master/app/src/main/java/uk/ac/qub/eeecs/game/matchAttax/cards/ManagerCard.java
+++ Gage-master/app/src/main/java/uk/ac/qub/eeecs/game/matchAttax/cards/ManagerCard.java
@@ -2,6 +2,11 @@ package uk.ac.qub.eeecs.game.matchAttax.cards;
 
 import android.graphics.Bitmap;
 
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import uk.ac.qub.eeecs.gage.engine.AssetManager;
 import uk.ac.qub.eeecs.gage.world.GameScreen;
 
 public class ManagerCard extends Card {
@@ -15,9 +20,28 @@ public class ManagerCard extends Card {
     private boolean type; //Addition card or a multiplier (true = add // false = multiply)
     private Cards[] cardsInStore = new Cards[amountOfCardsInDatabase];
 
-    public ManagerCard(GameScreen gameScreen, int overallValue, String firstName, String surname, Bitmap cardPortrait){
+    public ManagerCard(GameScreen gameScreen, int overallValue, String firstName, String surname, Bitmap cardPortrait) {
         super(gameScreen, overallValue, firstName, surname, cardPortrait);
 
+        AssetManager assetManager = gameScreen.getGame().getAssetManager();
+        String jsonToBeAdded = "";
+
+        try {
+            jsonToBeAdded = assetManager.getFileIO().loadJSON("txt/assets/Managers.json");
+
+        } catch (IOException ioEx) {
+            System.out.println(ioEx.getMessage());
+        }
+
+        try {
+            JSONObject manager = new JSONObject(jsonToBeAdded);
+
+        //    if (value == FALSE)
+
+        }catch(Exception ioEx){
+            System.out.println(ioEx.getMessage());
+
+        }
 
     }
 }

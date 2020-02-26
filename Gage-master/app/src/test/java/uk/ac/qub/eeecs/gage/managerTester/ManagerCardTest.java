a5d9c140795da51b0dde0ade090eb1c44003b16d
davidm9528@gmail.com <davidm9528@gmail.com>
2020-02-26 13:27:15 +0000

    Removed unnecessary imports

--- Gage-master/app/src/test/java/uk/ac/qub/eeecs/gage/managerTester/ManagerCardTest.java
+++ Gage-master/app/src/test/java/uk/ac/qub/eeecs/gage/managerTester/ManagerCardTest.java
@@ -1,35 +1,20 @@
 //Add into next sprint
 /*
 import android.content.Context;
-//package uk.ac.qub.eeecs.gage.managerTester;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.internal.matchers.Null;
-import org.mockito.runners.MockitoJUnitRunner;
 
 import java.io.File;
-import java.util.List;
-import java.lang.Object;
-
-import uk.ac.qub.eeecs.gage.Game;
-import uk.ac.qub.eeecs.gage.engine.AssetManager;
 import uk.ac.qub.eeecs.gage.engine.io.FileIO;
 import uk.ac.qub.eeecs.game.DemoGame;
-import uk.ac.qub.eeecs.game.matchAttax.cards.ManagerCard;
-import uk.ac.qub.eeecs.game.matchAttax.player.Deck;
-import uk.ac.qub.eeecs.game.matchAttax.player.Player;
 import uk.ac.qub.eeecs.game.matchAttax.screens.MatchGameScreen;
 
 import static junit.framework.Assert.assertTrue;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 
 //40238376
-//@RunWith(AndroidUnit4.class)
+@RunWith(AndroidUnit4.class)
 public class ManagerCardTest {
 
     private Context context;
